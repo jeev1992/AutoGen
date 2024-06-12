70a8ada00be6570ac6c24a0fa025bf9b46fdbc92
Jeevendra Singh (from Dev Box) <jeesingh@microsoft.com>
2024-06-12 14:50:07 +0530

    Agent with tools

---
+++ AgentWithTools.py
@@ -0,0 +1,54 @@
+from typing import Annotated, Literal
+from autogen import ConversableAgent
+from config.llm_config import llm_config
+from autogen import register_function
+
+Operator = Literal["+", "-", "*", "/"]
+
+
+def calculator(a: int, b: int, operator: Annotated[Operator, "operator"]) -> int:
+    if operator == "+":
+        return a + b
+    elif operator == "-":
+        return a - b
+    elif operator == "*":
+        return a * b
+    elif operator == "/":
+        return int(a / b)
+    else:
+        raise ValueError("Invalid operator")
+
+# Let's first define the assistant agent that suggests tool calls.
+assistant = ConversableAgent(
+    name="Assistant",
+    system_message="You are a helpful AI assistant. "
+    "You can help with simple calculations. "
+    "Return 'TERMINATE' when the task is done.",
+    llm_config=llm_config,
+)
+
+# The user proxy agent is used for interacting with the assistant agent
+# and executes tool calls.
+user_proxy = ConversableAgent(
+    name="User",
+    llm_config=False,
+    is_termination_msg=lambda msg: msg.get("content") is not None and "TERMINATE" in msg["content"],
+    human_input_mode="NEVER",
+)
+
+# # Register the tool signature with the assistant agent.
+# assistant.register_for_llm(name="calculator", description="A simple calculator")(calculator)
+
+# # Register the tool function with the user proxy agent.
+# user_proxy.register_for_execution(name="calculator")(calculator)
+
+# Register the calculator function to the two agents.
+register_function(
+    calculator,
+    caller=assistant,  # The assistant agent can suggest calls to the calculator.
+    executor=user_proxy,  # The user proxy agent can execute the calculator calls.
+    name="calculator",  # By default, the function name is used as the tool name.
+    description="A simple calculator",  # A description of the tool.
+)
+
+chat_result = user_proxy.initiate_chat(assistant, message="What is (44232 + 13312 / (232 - 32)) * 5?")
