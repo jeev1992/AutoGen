54c5e57dddf9e6fae4287ddedb302d96e7cb8a37
Jeevendra Singh (from Dev Box) <jeesingh@microsoft.com>
2024-06-20 19:58:17 +0530

    comments updated

--- SimpleConversableAgent.py
+++ SimpleConversableAgent.py
@@ -5,7 +5,7 @@ agent = ConversableAgent(
     "chatbot",
     llm_config=llm_config,
     code_execution_config=False,  # Turn off code execution, by default it is off.
-    function_map=None,  # No registered functions, by default it is None.
+    function_map=None,  # No registered functions/APIs, by default it is None.
     human_input_mode="NEVER",  # Never ask for human input.
 )
 
